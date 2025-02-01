23374656b10265685108f634d7cfac2833ae6b63
JavalPatel2005 <mp740189@gmail.com>
2025-02-01 11:29:24 +0530

    CSS,JS COMPLETED

---
+++ assets/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5935d70cc5c231cd986ebfd8e8c5d39102939cf5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5935d70cc5c231cd986ebfd8e8c5d39102939cf5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.3.13',
+            'version' => '8.3.13.0',
+            'reference' => '9981b086f1463389cb0df0be59e90c1f64d430c2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
